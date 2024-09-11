3a48722b1c69c9bbf1ed366e465c5b4b86db4f4d
Nayanpatel48 <www.xil187@gmail.com>
2024-09-11 14:38:42 +0530

    comments added in constructor.sol file

--- one/constructor.sol
+++ one/constructor.sol
@@ -4,9 +4,12 @@ pragma solidity >= 0.5.0 < 0.9.0;
 
 ////////////////////////////////Constructor//////////////////////////////////
 //constructor will only executed once after pressing deploy 
+//compiler will atomatically create default constructor 4 u no need to 
+//explicitly create default constructor
+//There should be only one constructor
 
 contract local{
-    
+
     // state variable
     uint public count;
 
