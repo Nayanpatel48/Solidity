9acd6680ee8d29c6c976ad91ff02b4782497369e
Nayanpatel48 <www.xil187@gmail.com>
2024-09-11 19:37:42 +0530

    overflow concept clear

--- one/integer.sol
+++ one/integer.sol
@@ -5,6 +5,5 @@ pragma solidity >= 0.5.0 < 0.9.0;
 contract local{
     int8 count1=127;//-128 to 127 range 
     //int8 count2=128; error because out of range
-    int16 count2=7878;//work
-    
+    int16 count2=7878;//work   
 }
