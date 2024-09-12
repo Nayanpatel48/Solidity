43566dc6936f264981cb94f6407dbc15008c374b
Nayanpatel48 <www.xil187@gmail.com>
2024-09-12 14:10:00 +0530

    loops done

--- three/loops.sol
+++ three/loops.sol
@@ -2,3 +2,28 @@
 
 pragma solidity >= 0.5.0 < 0.9.0;
 
+//we can use loops only inside the function
+
+contract loops{
+    uint[3] public arr;
+    uint public count;
+
+    function whileLoop() public {
+        //while loop
+        while (count<arr.length){
+        arr[count]=count;
+        count++;
+        }
+    }
+    function forLoop() public {
+        for(uint i=0;i<arr.length;i++){
+            arr[i]=i;
+        }
+    }
+    function doWhile() public {
+        do{
+            arr[count]=count;
+            count++;
+        }while(count<arr.length);
+    }
+}
